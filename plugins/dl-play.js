82e841dbbae66131282b68308ea7e2d04dafd043
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-06-10 18:20:42 +0300

    дргш

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -2,20 +2,20 @@
 import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 	
-	if (!text) throw `✳️ Ingresa el título de una canción\n\n📌Ejemplo *${usedPrefix + command}* Lil Peep hate my life`
+	if (!text) throw `✳️ Введите название песни\n\n📌Пример *${usedPrefix + command}* Лил Пип ненавидит мою жизнь`
 	let res = await yts(text)
 	let vid = res.videos[0]
-	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+	if (!vid) throw `✳️ Видео/Аудио не найдено`
 	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
 	//const url = 'https://www.youtube.com/watch?v=' + videoId
-	m.react('🎧')
+	m.реагировать('🎧')
 	let play = `
-	≡ *FG MUSIC*
+	≡ *МУЗЫКА*
 ┌──────────────
-▢ 📌 *Título* : ${title}
-▢ 📆 *Publicado:* ${ago}
-▢ ⌚ *Duración:* ${timestamp}
-▢ 👀 *Vistas:* ${views}
+▢ 📌 *Заголовок* : ${title}
+▢ 📆 *Печатный:* ${ago}
+▢ ⌚ *Продолжительность:* ${timestamp}
+▢ 👀 *Вид:* ${views}
 └──────────────`
  await conn.sendButton(m.chat, play, fgig, thumbnail, [
     ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
