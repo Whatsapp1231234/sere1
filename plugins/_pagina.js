b732738f7256d2b2fbd4469f286cd79a03e6d214
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-06-10 13:50:18 +0300

    дшпо

--- plugins/_pagina.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, usedPrefix, command, paypal }) => {
-conn.sendHydrated(m.chat, paypal, null, null, 'https://www.paypal.me/AMxScan/', 'PAYPAL', null, null,[
-], m)
-}
-handler.help = ['pagina']
-handler.tags = ['info']
-handler.command = /^paypal$/i
-export default handler
