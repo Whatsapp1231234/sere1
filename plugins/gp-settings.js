01958d1a2a99309bfdde33e6d84098a559defb6c
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-06-13 14:33:10 +0300

    ждпр

--- plugins/gp-settings.js
+++ plugins/gp-settings.js
@@ -1,19 +1,19 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     let isClose = { // Switch Case Like :v
-        'open': 'not_announcement',
-        'close': 'announcement',
+        'открыть': 'not_announcement',
+        'закрыть': 'announcement',
     }[(args[0] || '')]
     if (isClose === undefined)
         throw `
-*✳️ Elija una opción:*
-  *▢ ${usedPrefix + command} close*
-  *▢ ${usedPrefix + command} open*
+*✳️ Выберите опцию:*
+  *▢ ${usedPrefix + command} закрыть*
+  *▢ ${usedPrefix + command} открыть*
 `.trim()
     await conn.groupSettingUpdate(m.chat, isClose)
 }
-handler.help = ['group *open/close*']
+handler.help = ['группу *открыть/закрыть*']
 handler.tags = ['group']
-handler.command = ['group', 'grupo'] 
+handler.command = ['group', 'группу'] 
 handler.admin = true
 handler.botAdmin = true
 
