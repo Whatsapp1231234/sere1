ef7e6071dba7181c3d99c59e11db4060e74ac581
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-06-13 11:37:37 +0300

    кнгш

--- plugins/fun-tomp3.js
+++ plugins/fun-tomp3.js
@@ -4,18 +4,18 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     try {
     let q = m.quoted ? m.quoted : m
    let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-   // if (!/video|audio/.test(mime)) throw `✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`
+   // if (!/video|audio/.test(mime)) throw `✳️ Rперейдите к видео или голосовой заметке, которую вы хотите преобразовать в mp3, с помощью команды :\n\n*${usedPrefix + command}*`
     let media = await q.download?.()
-    if (!media) throw '❎ Error al descargar medios'
+    if (!media) throw '❎ Ошибка при загрузке мультимедиа'
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw '❎ Error al convertir'
+    if (!audio.data) throw '❎Ошибка при преобразовании'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
     } catch (e) {
         m.reply(`✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`)
    }
 }
-handler.help = ['tomp3']
+handler.help = ['музыка']
 handler.tags = ['fun']
-handler.command = ['tomp3', 'mp3', 'toudio'] 
+handler.command = ['музыка', 'mp3', 'toudio'] 
 
 export default handler
