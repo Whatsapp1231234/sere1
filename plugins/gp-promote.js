38b1b281aa3ed3f2895a44fc0ce12e2a9db7496e
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-06-13 14:19:39 +0300

    арол

--- plugins/gp-promote.js
+++ plugins/gp-promote.js
@@ -6,8 +6,8 @@ var number = text.split`@`[1]
 }else if(!isNaN(text)) {
 var number = text
 }
-if(!text && !m.quoted) return conn.reply(m.chat, `✳️ Uso del comamdo \n *${usedPrefix + command}* @tag  (o responda un mensaje)`, m)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `✳️ Numero incorrecto`, m)
+if(!text && !m.quoted) return conn.reply(m.chat, `✳️ Использование комамды \n *${usedPrefix + command}* @tag  (или ответьте на сообщение)`, m)
+if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `✳️ Неправильный номер`, m)
 try {
 if(text) {
 var user = number + '@s.whatsapp.net'
@@ -19,7 +19,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-m.reply(`✅ Usuario promovido`)
+m.reply(`✅Продвигаемый пользователь`)
 }}
 handler.help = ['promote']
 handler.tags = ['group']
