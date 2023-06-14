f89f9a0814753a17c0ce13a7672378672362a2b3
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-06-14 12:41:36 +0300

    спроь

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,7 +2,7 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `✳️ *Ingrese número de serie*\nVerifique su número de serie con el comando...\n\n*${usedPrefix}nserie*`
+  if (!args[0]) throw `✳️ *Введите серийный номер*\пПроверьте свой серийный номер с помощью команды...\n\n*${usedPrefix}nserie*`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw '⚠️ *Número de serie incorrecto*'
